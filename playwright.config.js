f2d8bc82eed83ee1680fe8da904d32d132370799
Katsumi <freundist@gmail.com>
2026-01-06 16:52:47 +0900

    chore: re-run GitHub Actions

--- playwright.config.js
+++ playwright.config.js
@@ -2,6 +2,8 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
+  testMatch: ['**/e2e.spec.js'],
+  testIgnore: ['**/integration.test.js'],
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,

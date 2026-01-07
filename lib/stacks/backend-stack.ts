ffc50919a537194baed3c7119224635739b8c869
Katsumi-commits <freundist@gmail.com>
2026-01-07 14:36:17 +0900

    Auto PR: feature/autoTest (#13)
    * 最終追加
    
    * 朝のデプロイ
    
    * Json調整
    
    * 最終追加
    
    * CORS修正
    
    * CORS調整

--- lib/stacks/backend-stack.ts
+++ lib/stacks/backend-stack.ts
@@ -283,8 +283,8 @@ table = dynamodb.Table(os.environ['ISSUES_TABLE'])
 
 CORS_HEADERS = {
     'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
-    'Access-Control-Allow-Headers': 'Content-Type,Authorization'
+    'Access-Control-Allow-Methods': 'GET,PUT,DELETE,OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type'
 }
 
 def handler(event, context):
